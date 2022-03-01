4bc6d18607c682e5ed4f7999ad012d31ff3eb7b3
Amit1 <Amit1@Amit>
2022-03-01 06:19:13 -0800

    First commit

---
+++ TestProject/src/com/github/MainGit.java
@@ -0,0 +1,17 @@
+package com.github;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainGit {
+	public static void main(String[] args) {
+		
+		
+		List<String> list=new ArrayList<>();
+		
+		list.add("amit");
+		list.add("sujeet");
+		list.add("abhi");
+		System.out.println(list);
+	}
+}
