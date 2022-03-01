3a7f48138e07c27d07815ffa7ad35a6536d2626e
Amit1 <Amit1@Amit>
2022-03-01 08:22:32 -0800

    second commit

--- TestProject/src/com/github/Student.java
+++ TestProject/src/com/github/Student.java
@@ -4,39 +4,46 @@ public class Student {
 	private int id;
 	private String marks;
 	private String name;
+
 	public Student() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public Student(int id, String marks, String name) {
 		super();
 		this.id = id;
 		this.marks = marks;
 		this.name = name;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getMarks() {
 		return marks;
 	}
+
 	public void setMarks(String marks) {
 		this.marks = marks;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", marks=" + marks + ", name=" + name + "]";
 	}
-	
-	
 
 }
